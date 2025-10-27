2104b4b92fd36dfd80d4ebf49025374f60d027e7
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-10-27 21:03:56 +0000

    configure subnets and route tables

---
+++ terraform/modules/vpc/routes.tf
@@ -0,0 +1,43 @@
+# public subnet route table
+
+resource "aws_route_table" "public" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = var.igw_id
+  }
+}
+
+resource "aws_route_table_association" "public_zone1" {
+  subnet_id = aws_subnet.public_zone1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_zone2" {
+  subnet_id = aws_subnet.public_zone2.id
+  route_table_id = aws_route_table.public.id
+}
+
+# private subnet route tables
+
+resource "aws_route_table" "private" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+# assign route tables to corresponding private subnet in correct az
+
+resource "aws_route_table_association" "private_zone1" {
+  subnet_id      = aws_subnet.private_zone1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_zone2" {
+  subnet_id      = aws_subnet.private_zone2.id
+  route_table_id = aws_route_table.private.id
+}
