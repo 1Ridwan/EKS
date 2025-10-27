24b1e9bedd6840d5c2ba26226a88a200ce10e292
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-10-27 22:27:03 +0000

    add eks cluster resource

---
+++ terraform/modules/eks/eks-cluster.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_role" "eks" {
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks" {
+  role       = aws_iam_role.eks.name
+  policy_arn = "arn:aws:iam:aws:policy/AmazonEKSClusterPolicy"
+}
+
+# eks cluster resource
+
+resource "aws_eks_cluster" "eks" {
+  name = "cluster-project"
+  version = var.eks_version
+  role_arn = aws_iam_role.eks.arn
+
+  access_config {
+    authentication_mode = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  vpc_config {
+
+    endpoint_private_access = false
+    endpoint_public_access = true
+
+    subnet_ids = [
+      aws_subnet.private_zone1.id,
+      aws_subnet.private_zone2.id
+    ]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted
+  # after EKS Cluster handling. Otherwise, EKS will not be able to
+  # properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks,
+  ]
+}
+
