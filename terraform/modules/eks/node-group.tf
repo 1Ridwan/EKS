f764c1325c1f4c082c6a362547d9674b45f4560e
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-10-27 22:40:28 +0000

    add node group resource

---
+++ terraform/modules/eks/node-group.tf
@@ -0,0 +1,69 @@
+resource "aws_iam_role" "nodes" {
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy"" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = "arn:aws:iam:aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = "arn:aws:iam:aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry_read_only" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = "arn:aws:iam:aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
+resource "aws_eks_node_group" "general" {
+  cluster_name    = aws_eks_cluster.eks.name
+
+  node_role_arn   = aws_iam_role.nodes.arn
+  subnet_ids      = aws_subnet.example[*].id
+  subnet_ids = [
+        aws_subnet.private_zone1.id,
+        aws_subnet.private_zone2.id
+    ]
+
+
+
+
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 10
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.amazon_eks_cni_policy,
+    aws_iam_role_policy_attachment.amazon_ec2_container_registry_read_only,
+  ]
+
+  lifecyce {
+      ignore_changes = [scaling_config[0].desired_size]
+  }
+}
